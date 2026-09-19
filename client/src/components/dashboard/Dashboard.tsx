d030ebc13f5ae61b93c256ddcc5dd14519820a6f
shanilkamfernando <shanilkamfernando123@gmail.com>
2026-09-19 09:45:08 +0530

    add a new card

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -9,6 +9,7 @@ type PortalId =
   | "workshop"
   | "documents"
   | "jobAssigned"
+  | "myTasks"
   | "meetings"
   | "followup"
   | "staff";
@@ -52,6 +53,13 @@ const Dashboard = () => {
   };
 
   const portals: Portal[] = [
+    {
+      id: "myTasks",
+      label: "My Tasks",
+      icon: "⏱️",
+      path: "/myTasks",
+      description: "Specific Users Task management",
+    },
     {
       id: "jobAssigned",
       label: "Job Assigned",
